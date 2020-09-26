c4ff1710e7286ea0c5c4a978da55b2948cc96520
bhavaniravi <bhava0895@gmail.com>
2020-09-26 23:37:09 +0530

    remove unwanted dvc files

--- src/config.py
+++ src/config.py
@@ -1,10 +1,11 @@
 class Config:
-    ORIGINAL_DATASET_PATH = "assets/original_data"
+    BASE = "assets"
+    ORIGINAL_DATASET_PATH = f"{BASE}/original_data"
     TRAIN_DATASET = f"{ORIGINAL_DATASET_PATH}/train.csv"
     TEST_DATASET = f"{ORIGINAL_DATASET_PATH}/test.csv"
-    PREPROCESS_PATH = "assets/preprocessed"
-    FEATURES_PATH = "assets/featurized"
-    MODELS_PATH = "assets/models"
+    PREPROCESS_PATH = f"{BASE}/preprocessed"
+    FEATURES_PATH = f"{BASE}/featurized"
+    MODELS_PATH = f"{BASE}/models"
 
     PREPROCESSED_TRAIN = f"{PREPROCESS_PATH}/train.csv"
     PREPROCESSED_TEST = f"{PREPROCESS_PATH}/test.csv"
